db9d6bcd78cef3b41c4b15d8a390d77e2a6990b4
Shashaank Bomule <32075174+shashaankunt@users.noreply.github.com>
2021-01-01 18:42:29 -0500

    Create LeetCode498NextGreaterElement.java

---
+++ LeetCode498NextGreaterElement.java
@@ -0,0 +1,58 @@
+//Finding the next greater element to the right by using stacks in 0(n) time
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        
+        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+        
+        Stack<Integer> st=new Stack<Integer>();
+        int len2=nums2.length-1;
+        
+        for(int i=len2;i>=0;i--)
+        {
+            int curr=nums2[i];
+            if(st.isEmpty())
+            {
+                hm.put(curr,-1);
+                st.push(curr);
+            }
+            else if(st.peek()>curr)
+            {
+                hm.put(curr,st.peek());
+                st.push(curr);
+            }
+            else
+            {
+                while(!st.isEmpty())
+                {
+                    if(st.peek()>curr)
+                    {
+                        hm.put(curr,st.peek());
+                        st.push(curr);
+                        break;
+                    }
+                    st.pop();
+                }
+                if(st.isEmpty())
+                {
+                  hm.put(curr,-1);
+                    st.push(curr);
+                }
+                
+            }      
+            
+        }
+        System.out.println(hm);
+        int[] res =new int[nums1.length];
+        for(int i=0;i<=nums1.length-1;i++)
+        {
+            res[i]=hm.get(nums1[i]);
+            
+        }
+        
+        return res;
+        
+        
+    }
+}
