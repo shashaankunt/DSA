e906f15142517f0a17dcb4e7dea33b12669241f4
Shashaank Bomule <32075174+shashaankunt@users.noreply.github.com>
2021-01-23 23:12:15 -0500

    Create LeetCodeMoveZeros.java

---
+++ LeetCodeMoveZeros.java
@@ -0,0 +1,27 @@
+// Get the non index value
+// increase index and copy the elemebts ignoring 0's
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+     
+        int index=0;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[index++]=nums[i];
+            }
+            
+            
+        }
+        
+        for(int i=index;i<nums.length;i++)
+        {
+            nums[i]=0;
+        }
+        
+        
+        
+    }
+}
