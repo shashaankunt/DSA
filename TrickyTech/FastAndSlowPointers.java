c400b8f8a2d8c00f94e3d46f813923f6b698a99a
Shashaank Bomule <32075174+shashaankunt@users.noreply.github.com>
2021-02-23 22:21:05 -0500

    tortoise and hare--fast and slow pointers

--- TrickyTech/FastAndSlowPointers.java
+++ TrickyTech/FastAndSlowPointers.java
@@ -1,4 +1,17 @@
 
+287. Find the Duplicate Number
+Medium
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
 //Tortoise and hare problem
 class Solution {
     public int findDuplicate(int[] nums) {
