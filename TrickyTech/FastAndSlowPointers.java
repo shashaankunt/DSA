8de611be86c6fced322a080e8194d7a1b9c5edee
Shashaank Bomule <32075174+shashaankunt@users.noreply.github.com>
2021-02-23 22:20:23 -0500

    Create FastAndSlowPointers.java
    Tortoise and hare fast and slow

---
+++ TrickyTech/FastAndSlowPointers.java
@@ -0,0 +1,29 @@
+
+//Tortoise and hare problem
+class Solution {
+    public int findDuplicate(int[] nums) {
+       
+ 
+        int slow=nums[0];
+        int fast=nums[nums[0]];
+        
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            
+        }
+        
+         fast=0;
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+        
+        
+    }
+}
