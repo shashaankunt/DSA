ed2a50bd183d6955509ca677485174682ae82234
Shashaank Bomule <32075174+shashaankunt@users.noreply.github.com>
2021-02-23 21:39:48 -0500

    Create Duplicate.java
    Trick question--make values in arr negative to get the first dup num

---
+++ TrickyTech/Duplicate.java
@@ -0,0 +1,32 @@
+287. Find the Duplicate Number
+Medium
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+       
+        for(int i=0;i<nums.length;i++)
+        {
+            
+            int val=Math.abs(nums[i]);
+            
+            if(nums[val]<0) return val;
+            else nums[val]=-nums[val];
+            
+        }
+        return -1;
+        
+        
+        
+    }
+}
